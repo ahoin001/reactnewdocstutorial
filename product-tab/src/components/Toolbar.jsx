91f093e33d10a5ee9273419183742497d6862f46
Alexander <alexander@Alexanders-MacBook-Pro.local>
2022-03-10 23:59:16 -0500

    Changed to arrow functions

--- product-tab/src/components/Toolbar.jsx
+++ product-tab/src/components/Toolbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Toolbar({ onPlayMovie, onUploadImage }) {
+export const Toolbar = () => {
   return (
     <div>
       <Button onClick={onPlayMovie}>Play Movie</Button>
       <Button onClick={onUploadImage}>Upload Image</Button>
     </div>
   );
-}
+};
 
-function Button({ onClick, children }) {
+export const Button = ({ onClick, children }) => {
   return <button onClick={onClick}>{children}</button>;
-}
+};
